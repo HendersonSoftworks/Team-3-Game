f49ca93951df1612654e6782621432966dbe543c
Antonio Simoes <asimoes@epo.org>
2024-02-02 13:35:25 +0100

    Initial commit of start menu

---
+++ Assets/scripts/MainMenuActions.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuActions : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Actions when start game is selected
+    public void StartGame()
+    {
+        Debug.Log("Start game pressed");
+    }
+
+    // Open options screen
+    public void OpenOptions()
+    {
+        Debug.Log("Open options pressed");
+    }
+
+    // Open achievements screen
+    public void OpenAchievements()
+    {
+        Debug.Log("Open achievements pressed");
+    }
+
+    // Open credits screen
+    public void OpenCredits()
+    {
+        Debug.Log("Open credits pressed");
+    }
+
+    // Open options screen
+    public void ExitGame()
+    {
+        Debug.Log("Exit game pressed");
+        Application.Quit();
+    }
+}
---
+++ Assets/scripts/PlaySounds.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySounds : MonoBehaviour
+{
+    public AudioClip moveSound;
+    public AudioClip selectSound;
+
+    AudioSource sound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sound = GetComponent<AudioSource>();
+    }
+
+    // Triggers sound when users move between menu options
+    public void PlayMoveSound()
+    {
+        sound.PlayOneShot(moveSound);
+    }
+
+    // Triggers sound when users select a menu option
+    public void PlaySelectSound()
+    {
+        sound.PlayOneShot(selectSound);
+    }
+
+}
