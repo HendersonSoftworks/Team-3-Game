a867b19588a2be6e1d41965377c7e9c255a2f314
HendersonSoftworks <thomas.henderson@hendersonsoftworks.com>
2024-03-01 23:13:42 +0000

    new test scene and added George's music

---
+++ Assets/scripts/MusicManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    public AudioSource musicSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
